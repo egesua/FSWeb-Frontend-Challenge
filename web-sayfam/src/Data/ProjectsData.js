23f17d7b80bb394bd552f076c3790cf0a6f4d49c
egesua <egesu06@gmail.com>
2023-10-25 20:18:21 +0300

    final final update

--- web-sayfam/src/Data/ProjectsData.js
+++ web-sayfam/src/Data/ProjectsData.js
@@ -7,8 +7,8 @@ export const ProjectsData = [
     explanation:
     "The web page I designed for software developers who want to order pizza, also serves as my WorkinTech 8th-week challenge project.",
     libraries: ["react", "vercel", "axios", "router"],
-    gitHubLink: "https://github.com/berkakaz/fsweb-s7-challenge-pizza",
-    vercelLink: "https://berkakaz.vercel.app/",
+    gitHubLink: "https://github.com/egesua/fsweb-s7-challenge-pizza",
+    vercelLink: "https://egesua.vercel.app/",
     backgroundColor: "#DDEEFE",
   },
 ];
